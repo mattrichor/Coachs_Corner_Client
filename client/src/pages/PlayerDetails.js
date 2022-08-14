116976565bee31607a824f901ed598aa39680115
mattrichor <matt.a.geyer@gmail.com>
2022-08-14 17:41:06 -0400

    more styling on player page

--- client/src/pages/PlayerDetails.js
+++ client/src/pages/PlayerDetails.js
@@ -1,3 +1,5 @@
+import '../Details.css'
+
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
@@ -12,34 +14,37 @@ const PlayerDetails = ({ player }) => {
   return (
     <div className="player-page">
       <h1 className="player-name">{player.name}</h1>
-      <h3 className="player-email">{player.email}</h3>
+      <h6 className="player-email">{player.email}</h6>
       <div className="player-info">
         <div className="player-grid">
-          <div>
+          <div className="grid-top">
             <h4>Height</h4>
             <h2>{player.height}</h2>
           </div>
-          <div>
+          <div className="empty"></div>
+          <div className="grid-bottom">
             <h4>Age</h4>
             <h2>{player.age}</h2>
           </div>
         </div>
         <div className="player-grid">
-          <div>
+          <div className="grid-top">
             <h4>Weight</h4>
             <h2>{player.weight}</h2>
           </div>
-          <div>
+          <div className="empty"></div>
+          <div className="grid-bottom">
             <h4>Active</h4>
-            <h2>{player.isActive}</h2>
+            {player.isActive != true ? <h2>No</h2> : <h2>Yes</h2>}
           </div>
         </div>
         <div className="player-grid">
-          <div>
+          <div className="grid-top">
             <h4>Primary Position</h4>
             <h2>{player.primaryPosition}</h2>
           </div>
-          <div>
+          <div className="empty"></div>
+          <div className="grid-bottom">
             <h4>Secondary Position</h4>
             <h2>{player.secondaryPosition}</h2>
           </div>

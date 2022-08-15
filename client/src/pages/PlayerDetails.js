fb591e4fcde5c385c7a72b0a8a5b25d985064a72
mattrichor <matt.a.geyer@gmail.com>
2022-08-14 22:31:43 -0400

    styling player page

--- client/src/pages/PlayerDetails.js
+++ client/src/pages/PlayerDetails.js
@@ -7,9 +7,35 @@ import axios from 'axios'
 const PlayerDetails = ({ player }) => {
   let { playerId } = useParams()
 
-  //   useEffect(() => {
-  //     const getPlayerById =
-  //   })
+  let feetMath = (player.height / 12).toFixed(0)
+  let inchMath = player.height % 12
+
+  let secondaryPosAbr
+  switch (player.secondaryPosition) {
+    case 'Pitcher':
+      secondaryPosAbr = 'Pitcher'
+      break
+    case 'Catcher':
+      secondaryPosAbr = 'Catcher'
+      break
+    case 'First Baseman':
+      secondaryPosAbr = '1B'
+      break
+    case 'Second Baseman':
+      secondaryPosAbr = '2B'
+      break
+    case 'Third Baseman':
+      secondaryPosAbr = '3B'
+      break
+    case 'Short Stop':
+      secondaryPosAbr = 'SS'
+      break
+    case 'Center Fielder':
+      secondaryPosAbr = 'CF'
+      break
+    case 'Right Fielder':
+      secondaryPosAbr = 'RF'
+  }
 
   return (
     <div className="player-page">
@@ -19,7 +45,9 @@ const PlayerDetails = ({ player }) => {
         <div className="player-grid">
           <div className="grid-top">
             <h4>Height</h4>
-            <h2>{player.height}</h2>
+            <h2>
+              {feetMath}'{inchMath}"
+            </h2>
           </div>
           <div className="empty"></div>
           <div className="grid-bottom">
@@ -30,7 +58,7 @@ const PlayerDetails = ({ player }) => {
         <div className="player-grid">
           <div className="grid-top">
             <h4>Weight</h4>
-            <h2>{player.weight}</h2>
+            <h2>{player.weight} lbs</h2>
           </div>
           <div className="empty"></div>
           <div className="grid-bottom">
@@ -46,11 +74,19 @@ const PlayerDetails = ({ player }) => {
           <div className="empty"></div>
           <div className="grid-bottom">
             <h4>Secondary Position</h4>
-            <h2>{player.secondaryPosition}</h2>
+            <h2>{secondaryPosAbr}</h2>
           </div>
         </div>
       </div>
-      <div className="player-skills-and-pic"></div>
+      <div className="player-stats-and-pic">
+        <div className="stat-graph">
+          <h1 className="skills">Skills</h1>
+          <div className="skills-graph"></div>
+        </div>
+        <div className="pic-container">
+          <img className="pic" src={player.proPic}></img>
+        </div>
+      </div>
     </div>
   )
 }

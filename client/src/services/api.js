adfb489e4e520746391238ecd075988849b82595
mattrichor <matt.a.geyer@gmail.com>
2022-08-19 09:40:55 -0400

    added gif to player page

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001'
+// export const BASE_URL = 'http://localhost:3001'
 
-// export const BASE_URL = 'https://coachs-corner-backend.herokuapp.com/'
+export const BASE_URL = 'https://coachs-corner-backend.herokuapp.com/'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
